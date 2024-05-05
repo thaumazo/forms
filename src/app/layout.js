0294f647a2a475fc561ec7a27c923887fd4c94d6
Kenneth Spencer <ken-spencer@users.noreply.github.com>
2024-05-05 11:05:56 -0700

    - tweaked theme names - fixed bug where components were not importing

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="autoTheme">
+      <body className="system">
         <Navigation />
         {children}
       </body>
